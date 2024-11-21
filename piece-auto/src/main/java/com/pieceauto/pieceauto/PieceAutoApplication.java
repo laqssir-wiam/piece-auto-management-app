9d411f8ffea54e8f7b6d1d0ab1480120619dc55b
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2024-11-21 22:16:40 +0100

    create backend project

---
+++ piece-auto/src/main/java/com/pieceauto/pieceauto/PieceAutoApplication.java
@@ -0,0 +1,13 @@
+package com.pieceauto.pieceauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieceAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PieceAutoApplication.class, args);
+	}
+
+}
---
+++ piece-auto/src/test/java/com/pieceauto/pieceauto/PieceAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pieceauto.pieceauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieceAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
