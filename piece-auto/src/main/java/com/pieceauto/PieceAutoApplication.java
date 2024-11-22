d641f166554453b611cb3a1db317b730615926c9
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2024-11-22 21:39:15 +0100

    changing global package

--- piece-auto/src/main/java/com/pieceauto/PieceAutoApplication.java
+++ piece-auto/src/main/java/com/pieceauto/PieceAutoApplication.java
@@ -1,4 +1,4 @@
-package com.pieceauto.pieceauto;
+package com.pieceauto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- piece-auto/src/test/java/com/pieceauto/PieceAutoApplicationTests.java
+++ piece-auto/src/test/java/com/pieceauto/PieceAutoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pieceauto.pieceauto;
+package com.pieceauto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
