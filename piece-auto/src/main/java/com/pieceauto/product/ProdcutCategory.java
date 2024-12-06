3c64cfa4c52a0528515ea6490395bd7f8ba6449f
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2024-12-06 20:22:11 +0100

    adding category class

--- piece-auto/src/main/java/com/pieceauto/product/ProdcutCategory.java
+++ piece-auto/src/main/java/com/pieceauto/product/ProdcutCategory.java
@@ -1,7 +1,20 @@
 package com.pieceauto.product;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 /**
  * @author WIAM
  **/
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
 public class ProdcutCategory {
+
 }
