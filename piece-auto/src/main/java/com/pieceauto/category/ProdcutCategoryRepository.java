c9ef0e2f8ef48dd3b467f9c252a6d6974d12a498
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2024-12-09 21:22:21 +0100

    implimenting ProdcutCategoryRepo and begining of th product service

---
+++ piece-auto/src/main/java/com/pieceauto/category/ProdcutCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.pieceauto.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author WIAM
+ **/
+public interface ProdcutCategoryRepository extends JpaRepository<ProdcutCategory,Integer> {
+}
