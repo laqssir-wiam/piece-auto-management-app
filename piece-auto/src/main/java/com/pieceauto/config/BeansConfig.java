26315f8f81943b080c9bfd51802e2361042e1709
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2025-01-01 20:14:32 +0100

    implimenting authenticationprovider

---
+++ piece-auto/src/main/java/com/pieceauto/config/BeansConfig.java
@@ -0,0 +1,25 @@
+package com.pieceauto.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * @author WIAM
+ **/
+@Configuration
+@RequiredArgsConstructor
+public class BeansConfig {
+    private final UserDetailsService userDetailsService;
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+}
