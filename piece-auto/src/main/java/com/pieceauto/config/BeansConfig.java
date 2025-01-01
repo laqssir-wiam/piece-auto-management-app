40ba9111ef0db69380ade6e0b55cd4d16df97ef4
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2025-01-01 20:15:01 +0100

    adding password encoder method

--- piece-auto/src/main/java/com/pieceauto/config/BeansConfig.java
+++ piece-auto/src/main/java/com/pieceauto/config/BeansConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author WIAM
@@ -21,5 +23,8 @@ public class BeansConfig {
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
