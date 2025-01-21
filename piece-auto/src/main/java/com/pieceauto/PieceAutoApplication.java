618aa2b731e13ed9b96322fbea21f13e3d0a0488
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2025-01-21 22:57:33 +0100

    adding email service

--- piece-auto/src/main/java/com/pieceauto/PieceAutoApplication.java
+++ piece-auto/src/main/java/com/pieceauto/PieceAutoApplication.java
@@ -3,9 +3,11 @@ package com.pieceauto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableAsync
 public class PieceAutoApplication {
 
 	public static void main(String[] args) {
