f5da3c89db71511fccbf98877c0da9c3fc73911f
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2025-01-23 21:45:55 +0100

    findProductById in contrller

--- piece-auto/src/main/java/com/pieceauto/product/ProductController.java
+++ piece-auto/src/main/java/com/pieceauto/product/ProductController.java
@@ -5,10 +5,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author WIAM
@@ -26,5 +23,11 @@ public class ProductController {
             Authentication connectedUser
     ) {
         return ResponseEntity.ok(service.saveProduct(request).getId());
+    }
+    @GetMapping("/{product-id}")
+    public ResponseEntity<Product> findBookById(
+            @PathVariable("product-id") Integer productId
+    ) {
+        return ResponseEntity.ok(service.findById(productId));
     }
 }
--- piece-auto/src/main/java/com/pieceauto/product/ProductService.java
+++ piece-auto/src/main/java/com/pieceauto/product/ProductService.java
@@ -25,14 +25,14 @@ public class ProductService {
     public Product saveProduct(ProductRequest productRequest){
         return  productRepository.save(productMapper.toProduct(productRequest));
     }
-    public Optional<Product> findById(Integer id){
-        return productRepository.findById(id);
+    public Product findById(Integer id){
+        return productRepository.findById(id).get();
     }
     public List<Product> findAll(){
         return productRepository.findAll();
     }
     public Product updateProduct(ProductRequest productRequest, Integer idProd){
-        Product product = findById(idProd).get();
+        Product product = findById(idProd);
         product.setPrice(productRequest.price());
         product.setMargin_price(productRequest.margin_price());
         product.setLabel(productRequest.label());
