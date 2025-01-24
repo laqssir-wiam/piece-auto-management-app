c1a0e0e4253668767b2ad424db8687193b22707c
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2025-01-24 22:14:56 +0100

    findAllProducts

--- piece-auto/src/main/java/com/pieceauto/product/ProductController.java
+++ piece-auto/src/main/java/com/pieceauto/product/ProductController.java
@@ -1,5 +1,6 @@
 package com.pieceauto.product;
 
+import com.pieceauto.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,16 +19,24 @@ public class ProductController {
     private final ProductService service;
 
     @PostMapping
-    public ResponseEntity<Integer> saveBook(
+    public ResponseEntity<Integer> saveProduct(
             @Valid @RequestBody ProductRequest   request,
             Authentication connectedUser
     ) {
         return ResponseEntity.ok(service.saveProduct(request).getId());
     }
     @GetMapping("/{product-id}")
-    public ResponseEntity<Product> findBookById(
+    public ResponseEntity<Product> findProductById(
             @PathVariable("product-id") Integer productId
     ) {
         return ResponseEntity.ok(service.findById(productId));
+    }
+    @GetMapping
+    public ResponseEntity<PageResponse<Product>> findAllBooks(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.findAll(page, size, connectedUser));
     }
 }
