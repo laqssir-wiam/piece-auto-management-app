857592053f2e4c35a24655df025a6c28de9462fd
LAQSSIR Wiam <laqssir.wiam@gmail.com>
2025-02-15 23:16:57 +0100

    adding cors for ui and bend relation

--- piece-auto/src/main/java/com/pieceauto/product/ProductService.java
+++ piece-auto/src/main/java/com/pieceauto/product/ProductService.java
@@ -69,8 +69,6 @@ public class ProductService {
     public ProductCategory getCatOfProduct(Integer idProd){
         return productRepository.findById(idProd).get().getProdcutCategory();
     }
-
-
     public Product getByRef(String ref){
         return productRepository.findByRef(ref);
     }
